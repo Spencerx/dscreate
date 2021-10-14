66f49c551e7b6ccc5818f3243610f83663b28e26
Joél <joelsewhere@gmail.com>
2021-10-14 10:05:08 -0500

    old argument added to EditApp to ensure notebooks can be merged if split with old splitting code

--- dscreate/pipeline/converters/MergeConverter.py
+++ dscreate/pipeline/converters/MergeConverter.py
@@ -1,10 +1,10 @@
-from .BaseConverter import BaseConverter
-from ..preprocessors import *
 import io
 import os
+from .BaseConverter import BaseConverter
+from ..preprocessors import *
 from nbformat import read
 from nbformat.v4 import new_notebook
-from traitlets import default, List, Unicode
+from traitlets import List, Unicode, Bool
 
 
 class MergeConverter(BaseConverter):
@@ -17,6 +17,7 @@ class MergeConverter(BaseConverter):
 
     preprocessors = List([SortCells]).tag(config=True)
     output = Unicode(u'curriculum').tag(config=True)
+    old = Bool(False).tag(config=True)
 
     def read_notebook(self, filepath):
         with io.open(filepath, mode="r", encoding="utf-8") as file:
@@ -37,9 +38,23 @@ class MergeConverter(BaseConverter):
         lesson_nb = self.read_notebook(lesson)
         solution_nb = self.read_notebook(solution)
 
+        # Add solution metadata to notebooks 
+        # That were split with old version of dscreate
+        if self.old:
+            for idx, cell in enumerate(lesson_nb.cells):
+                cell['metadata']['solution'] = False
+                lesson_nb.cells[idx] = cell
+
+            for idx, cell in enumerate(solution_nb.cells):
+                cell['metadata']['solution'] = True
+                solution_nb.cells[idx] = cell
+
         # Concatenate cells
         nb.cells.extend(lesson_nb.cells)
         nb.cells.extend(solution_nb.cells)
+
+        if self.old:
+            nb.cells = [cell for cell in nb.cells if cell['metadata']['index'] != 'Placeholder']
 
         # Setup BaseConverter's write_notebook
         self.config.source_notebook = nb
