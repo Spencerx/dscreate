37b3594a60d94fd7a4c27cb1e6d245a062c5c748
Joél <joelsewhere@gmail.com>
2021-10-14 17:35:06 -0500

    MarkdownApp

---
+++ dscreate/apps/MarkdownApp.py
@@ -0,0 +1,35 @@
+from .BaseApp import DsCreate
+from traitlets import List
+from traitlets.config import Config
+from ..pipeline import ReadmeConverter, DsPipeline
+
+aliases = {
+    'notebook':'ReadmeConverter.notebook_path',
+    'output': 'ReadmeConverter.output'
+    }
+
+class MarkdownApp(DsCreate):
+
+    name = 'MarkdownApp'
+    description = ''
+
+    pipeline_steps = List([
+
+        ReadmeConverter,
+
+        ]).tag(config=True)
+
+    def start(self) -> None:
+        super().start()
+
+        if len(self.extra_args) > 1:
+            raise ValueError("Only one argument (the notebook path) may be specified")
+        elif len(self.extra_args) == 0:
+            raise ValueError("A notebook path must be specified, either as an argument or with --notebook")
+    
+        c = Config()
+        c.DsPipeline.steps = self.pipeline_steps
+        c.ReadmeConverter.notebook_path = self.extra_args[0]
+        self.config.merge(c)
+        pipeline = DsPipeline(config=self.config)
+        pipeline.start()
